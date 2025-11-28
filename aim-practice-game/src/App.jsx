1b5dd373e95a68e0eeb651514214ddb393372c30
Alva Lundberg <alva.lundberg@hotmail.se>
2025-11-28 10:12:37 +0100

    Theme function added
    Theme.jsx is removed and moved into Target.jsx instead

--- aim-practice-game/src/App.jsx
+++ aim-practice-game/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import API from "./components/API.jsx";
 import Target from "./components/Target.jsx";
-import Theme from './components/Theme.jsx';
+
 function App() {
   return (
     <>
       <API/>
       <Target/>
-      <Theme/>
     </>
   )
 }
--- aim-practice-game/src/components/Target.jsx
+++ aim-practice-game/src/components/Target.jsx
@@ -1,10 +1,34 @@
 {/* In this component, the target (circles will appear) */}
 import "../target.css"
+import React from "react";
+import { useEffect, useState } from "react";
+
 function Target (){
+    const [color, setColor] = useState("blue");
+    const colors = ["red", "orange", "yellow", "blue", "indigo", "violet"]
+    const changeColor = () => {
+            setColor((current) => {
+                const indexColor = colors.indexOf(current);
+                const nextColor = (indexColor +1) % colors.length;
+                return colors[nextColor];
+            });
+        }
+        useEffect(() => {
+            document.title = `Theme ${color}`
+        }, [color]);
     return(
     <>
-        <div className="box-target">
-            <h1>h1</h1>
+        <div className="box-target" style={{backgroundColor: color}}> 
+        </div>
+        <div className="contiainer-theme">
+            <div className="">
+                <p className="theme">
+                    Theme: {color}</p>
+            </div>
+            <div className="buttons">
+
+            </div>
+            <button className="btn btn-color" onClick= {changeColor}>Change Theme</button>
         </div>
     </>
 )};
